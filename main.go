7e6f94cec7d5a611c3a02d04f752532f7c8578ec
alexey-y-a <lexus2157@mail.ru>
2025-08-06 23:56:23 +0300

    step_001

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"weathercli/geo"
+)
+
+func main() {
+	city := flag.String("city", "", "Город пользователя")
+	// format := flag.Int("format", 1, "Формат вывода погоды")
+
+	flag.Parse()
+
+	fmt.Println(*city)
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(geoData)
+
+}
